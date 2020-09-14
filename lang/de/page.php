f351c2af419eddecb3a50fad7acbc6e5b39d1190
Niels Seidel <niels.seidel@fernuni-hagen.de>
2020-09-14 09:06:23 +0200

    Language Extentsion

---
+++ lang/de/page.php
@@ -0,0 +1,60 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'page', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   mod_page
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['configdisplayoptions'] = 'Select all options that should be available, existing settings are not modified. Hold CTRL key to select multiple fields.';
+$string['content'] = 'Longoage Inhalt';
+$string['contentheader'] = 'Inhalt';
+$string['createpage'] = 'Eine neue Longpage anlegen';
+$string['displayoptions'] = 'Verfügbare Anzeigeoptionen';
+$string['displayselect'] = 'Darstellung';
+$string['displayselectexplain'] = 'Darstellungstyp wählen';
+$string['indicator:cognitivedepth'] = 'Longpage Verarbeitungstiefe';
+$string['indicator:cognitivedepth_help'] = 'Gibt die Verarbeitungtiefe für Lernende an';
+$string['indicator:socialbreadth'] = 'Longpage social';
+$string['indicator:socialbreadth_help'] = 'This indicator is based on the social breadth reached by the student in a Page resource.';
+$string['legacyfiles'] = 'Migration of old course file';
+$string['legacyfilesactive'] = 'Aktive';
+$string['legacyfilesdone'] = 'Abgeschlossen';
+$string['modulename'] = 'Longpage';
+$string['modulename_help'] = 'Longpage ermöglocht Lehrenden länge Tetxte in Form von HTML abzulegen.';
+
+$string['modulename_link'] = 'mod/page/view';
+$string['modulenameplural'] = 'Seiten';
+$string['optionsheader'] = 'Darstellungsoptionen';
+$string['page-mod-page-x'] = 'Jede Page/Longpage';
+$string['page:addinstance'] = 'Neue Seite als Ressource hinzufügen';
+$string['page:view'] = 'Seiteninhalt anzeigen';
+$string['pluginadministration'] = 'Page administrieren';
+$string['pluginname'] = 'Longpage';
+$string['popupheight'] = 'Pop-up Höhe (in Pixeln)';
+$string['popupheightexplain'] = 'Gibt die Standardhöhe von Pop-up Fenstern an.';
+$string['popupwidth'] = 'Pop-up Breite (in Pixeln)';
+$string['popupwidthexplain'] = 'Gibt die Standardbreite von Pop-up Fenstern an.';
+$string['printheading'] = 'Titel der Seite anzeigen';
+$string['printheadingexplain'] = 'Seitennamen oberhalb des Inhalts anzeigen?';
+$string['printintro'] = 'Seitenbeschreibung anzeigen?';
+$string['printintroexplain'] = 'Seitenbeschreibung oberhalb des Inhalts anzeigen?';
+$string['privacy:metadata'] = 'Longpage speichert auch personenbezogene Daten.';
+$string['search:activity'] = 'Longpage';
