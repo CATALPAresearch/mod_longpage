af222e2c7947fc6480f4d041ef6cb5fc094cde40
Adrian Stritzinger <Adrian.Stritzinger@web.de>
2020-11-04 23:48:03 +0100

    Add vuex store for annotations

--- vue/src/store/modules/annotation-module.js
+++ vue/src/store/modules/annotation-module.js
@@ -1,8 +1,46 @@
 import ajax from 'core/ajax';
+import {GET, ACT, MUTATE} from '../types';
 
 export default {
-    state: {},
-    getters: {},
-    actions: {},
-    mutations: {},
+    state: {
+        annotations: [],
+    },
+    getters: {
+        [GET.ANNOTATIONS]: ({annotations}) => annotations,
+    },
+    actions: {
+        [ACT.CREATE_ANNOTATION]({commit, getters}, annotation) {
+            ajax.call([{
+                methodname: 'mod_page_create_annotation',
+                args: {
+                    data: annotation,
+                },
+                done: (_) => {
+                    commit(MUTATE.SET_ANNOTATIONS, [getters[GET.ANNOTATIONS], annotation])
+                },
+                fail: (e) => {
+                    console.error('"mod_page_create_annotation" failed', e);
+                }
+            }]);
+        },
+        [ACT.READ_ANNOTATIONS]({commit}) {
+            ajax.call([{
+                methodname: 'mod_page_get_annotations_by_page',
+                done: (annotations) => {
+                    commit(MUTATE.SET_ANNOTATIONS, annotations)
+                },
+                fail: (e) => {
+                    console.error('"mod_page_get_annotations_by_page" failed', e);
+                }
+            }]);
+        },
+    },
+    mutations: {
+        [MUTATE.SET_ANNOTATIONS](state, annotations) {
+            state.annotations = annotations;
+        },
+        [MUTATE.RESET_ANNOTATIONS](state) {
+            state.annotations = [];
+        }
+    },
 }
---
+++ vue/src/store/types.js
@@ -0,0 +1,13 @@
+export const GET = Object.freeze({
+  ANNOTATIONS: 'Get Annotations',
+});
+
+export const ACT = Object.freeze({
+  CREATE_ANNOTATION: 'Create Annotation',
+  READ_ANNOTATIONS: 'Read Annotations',
+});
+
+export const MUTATE = Object.freeze({
+  RESET_ANNOTATIONS: 'Reset Annotations',
+  SET_ANNOTATIONS: 'Set Annotations',
+});
