9ae708033ea4e600b26140f68ad897a0b59f3ca0
Adrian Stritzinger <Adrian.Stritzinger@web.de>
2021-02-08 00:35:14 +0100

    Make sidebar resizeable

--- vue/src/components/longpage-sidebar/index.vue
+++ vue/src/components/longpage-sidebar/index.vue
@@ -1,12 +1,16 @@
 <template>
   <div
-    id="longpage-sidebar"
+    :id="LONGPAGE_SIDEBAR_ID"
     class="row no-gutters vh-100-wo-nav mw-75"
   >
     <div
       v-show="tabOpenedKey"
-      id="longpage-sidebar-tab-content"
-      class="col h-100"
+      class="resize-handle--x h-100 col-auto border-left border-right"
+    />
+    <div
+      v-show="tabOpenedKey"
+      :id="LONGPAGE_SIDEBAR_TAB_CONTENT"
+      class="col h-100 min-w-320-px mw-100"
     >
       <component
         :is="tab.key"
@@ -37,11 +41,50 @@
 </template>
 
 <script>
-import AnnotationSidebar from '@/components/longpage-sidebar/AnnotationSidebar';
 import {GET, MUTATE} from '@/store/types';
 import {mapGetters, mapMutations} from 'vuex';
-import {SidebarTabKeys} from '@/config/constants';
+import AnnotationSidebar from '@/components/longpage-sidebar/AnnotationSidebar';
+import {debounce} from 'lodash';
+import {LONGPAGE_APP_ID, SidebarTabKeys} from '@/config/constants';
 import TableOfContents from '@/components/longpage-sidebar/TableOfContents';
+
+const LONGPAGE_SIDEBAR_ID = 'longpage-sidebar';
+const LONGPAGE_SIDEBAR_TAB_CONTENT = 'longpage-sidebar-tab-content';
+
+const resizeData = {
+  tracking: false,
+  startWidth: undefined,
+  startCursorScreenX: undefined,
+  handleWidth: 10,
+  resizeTarget: null,
+  parentElement: null,
+  maxWidth: undefined,
+};
+
+$(document.body).on('mousedown', '.resize-handle--x', null, event => {
+  if (event.button !== 0) return;
+
+  const targetElement = document.getElementById(LONGPAGE_SIDEBAR_TAB_CONTENT);
+  resizeData.startWidth = $(targetElement).outerWidth();
+  resizeData.startCursorScreenX = event.screenX;
+  resizeData.resizeTarget = targetElement;
+  resizeData.parentElement = document.getElementById(LONGPAGE_APP_ID);
+  resizeData.maxWidth = $(resizeData.parentElement).innerWidth() - resizeData.handleWidth;
+  resizeData.tracking = true;
+});
+
+$(window).on('mousemove', null, null, debounce($event => {
+  if (resizeData.tracking) {
+    const cursorScreenXDelta = resizeData.startCursorScreenX - $event.screenX;
+    const newWidth = Math.min(resizeData.startWidth + cursorScreenXDelta, resizeData.maxWidth);
+
+    $(resizeData.resizeTarget).outerWidth(newWidth);
+  }
+}, 1));
+
+$(window).on('mouseup', null, null, () => {
+  if (resizeData.tracking) resizeData.tracking = false;
+});
 
 export default {
   name: 'LongpageSidebar',
@@ -51,6 +94,8 @@ export default {
   },
   data() {
     return {
+      LONGPAGE_SIDEBAR_ID,
+      LONGPAGE_SIDEBAR_TAB_CONTENT,
       tabs: [
         {key: SidebarTabKeys.ANNOTATIONS, id: 'sidebar-tab-annotations', icon: ['fa', 'fa-comment']},
         {key: SidebarTabKeys.TOC, id: 'sidebar-tab-table-of-contents', icon: ['fa', 'fa-list']},
@@ -59,6 +104,9 @@ export default {
   },
   computed: {
     ...mapGetters({tabOpenedKey: GET.SIDEBAR_TAB_OPENED_KEY}),
+  },
+  mounted() {
+
   },
   methods: {
     toggleTab(tab) {
@@ -69,3 +117,58 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="scss">
+  .mw-75 {
+    max-width: 75%;
+  }
+
+  .min-w-320-px {
+    min-width: 320px;
+  }
+
+  $handle-size: 10px;
+  $handle-thickness: 1px;
+  $handle-distance: 2px;
+
+  .resize-handle--x {
+    position: relative;
+    box-sizing: border-box;
+    width: 3px;
+    cursor: ew-resize;
+
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+
+    &:before {
+       content: "";
+       position: absolute;
+       z-index: 1;
+       top: 50%;
+       right: 100%;
+       height: $handle-size;
+       width: $handle-distance;
+       margin-top: -$handle-size/2;
+       border-left-color: black;
+       border-left-width: $handle-thickness;
+       border-left-style: solid;
+     }
+    &:after {
+       content: "";
+       position: absolute;
+       z-index: 1;
+       top: 50%;
+       left: 100%;
+       height: $handle-size;
+       width: $handle-distance;
+       margin-top: -$handle-size/2;
+       border-right-color: black;
+       border-right-width: $handle-thickness;
+       border-right-style: solid;
+     }
+  }
+</style>
