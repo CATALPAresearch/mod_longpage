18e4107ba2df375e7cf705a65f253f1b76da8e9d
Adrian Stritzinger <Adrian.Stritzinger@web.de>
2021-03-07 23:49:43 +0100

    Up plugin version for correct install

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021020705;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2021030700;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018050800;    // Requires this Moodle version
 $plugin->component = 'mod_page';       // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
