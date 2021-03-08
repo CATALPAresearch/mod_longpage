5649c77b500a9e9b8efdde067f3cca825020a5fa
Adrian Stritzinger <Adrian.Stritzinger@web.de>
2021-03-08 22:49:12 +0100

    Fix author search input for filtering posts

--- vue/src/util/fuse.js
+++ vue/src/util/fuse.js
@@ -1,19 +1,23 @@
 import {set} from 'lodash';
 
-export const mapResultToHighlightedDoc = ({item, matches}, hlStartTag = '<b>', hlEndTag = '</b>') => ({
-    ...item,
+const hlStartTag = '<b>';
+const hlEndTag = '</b>';
+
+export const mapResultToHighlightedDoc = ({item, matches}) => {
+    return {...item,
     ...matches.reduce((itemHighlighted, {indices, value, key}) => {
-        let currentValueIndex = 0;
-        return set(itemHighlighted, key, indices.reduce((highlight, [startIdx, endIdx], i) => {
-            const result = [
-                highlight,
-                value.slice(currentValueIndex, startIdx),
-                hlStartTag,
-                value.slice(startIdx, endIdx + 1),
-                hlEndTag,
-            ].join('');
-            currentValueIndex = endIdx + 1;
-            return i === indices.length - 1 ? [result, value.slice(currentValueIndex)].join('') : result;
-        }, ''));
-    }, {}),
-});
+            let currentValueIndex = 0;
+            return set(itemHighlighted, key, indices.reduce((highlight, [startIdx, endIdx], i) => {
+                const result = [
+                    highlight,
+                    value.slice(currentValueIndex, startIdx),
+                    hlStartTag,
+                    value.slice(startIdx, endIdx + 1),
+                    hlEndTag,
+                ].join('');
+                currentValueIndex = endIdx + 1;
+                return i === indices.length - 1 ? [result, value.slice(currentValueIndex)].join('') : result;
+            }, ''));
+        }, {}),
+    };
+};
