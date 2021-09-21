58c3b0a82e0f39534ca1ba222965a6ff3de7b4a8
KonstantinFriedrich <konstantfriedrich@gmail.com>
2021-09-21 12:52:12 +0200

    search result higlighting and adjustments

--- vue/src/components/LongpageSidebar/Search.vue
+++ vue/src/components/LongpageSidebar/Search.vue
@@ -1,8 +1,12 @@
 <script>
 import elasticlunr from "elasticlunr";
-import { LONGPAGE_APP_CONTAINER_ID, SidebarTabKeys, SidebarEvents } from "@/config/constants";
+import Fuse from "fuse.js";
+import {
+  LONGPAGE_APP_CONTAINER_ID,
+  SidebarTabKeys,
+  SidebarEvents,
+} from "@/config/constants";
 import { EventBus } from "@/lib/event-bus";
-
 
 export default {
   name: "Search",
@@ -15,11 +19,12 @@ export default {
     return {
       SidebarTabKeys,
       SidebarEvents,
-      index: {},
+      index: [],
       search: "",
       searchResults: "",
       searchTerm: "",
       showSearchResults: false,
+      fuse: null,
     };
   },
 
@@ -30,17 +35,18 @@ export default {
   methods: {
     setupSearch: function () {
       let _this = this;
+      let DocObj = new Object();
 
-      var customized_stop_words = ["an", "der", "die", "das"]; // add German stop words
-      elasticlunr.addStopWords(customized_stop_words);
+      // var customized_stop_words = ["an", "der", "die", "das"]; // add German stop words
+      // elasticlunr.addStopWords(customized_stop_words);
 
-      _this.index = elasticlunr();
-      //index.use(de);
-      _this.index.addField("title");
-      _this.index.addField("body");
-      _this.index.setRef("id");
+      // _this.index = elasticlunr();
+      // //index.use(de);
+      // _this.index.addField("title");
+      // _this.index.addField("body");
+      // _this.index.setRef("id");
       // collect index
-                            //commented section below because it produces double data entries
+      //commented section below because it produces double data entries
       // $(
       //   "#longpage-app h2, #longpage-app h3, #longpage-app h4, #longpage-app div, #longpage-app p, #longpage-app ul, #longpage-app ol, #longpage-app pre"
       // ).each(function (i, val) {
@@ -55,89 +61,148 @@ export default {
       $(
         "#longpage-app h2, #longpage-app h3, #longpage-app h4, #longpage-app div, #longpage-app p, #longpage-app ul, #longpage-app ol, #longpage-app pre"
       ).each(function (i, val) {
+        DocObj = new Object();
         if ($(this).is("h2") || $(this).is("h3") || $(this).is("h4")) {
-          _this.index.addDoc({
-            id: i,
-            title: $(val).text(),
-            body: "",
-            link: $(val).attr("id"),
-          });
+          DocObj.id = i;
+          DocObj.title = $(val).text();
+          DocObj.body = "";
+          DocObj.link = $(val).attr("id");
+          // _this.index.addDoc({
+          //   id: i,
+          //   title: $(val).text(),
+          //   body: "",
+          //   link: $(val).attr("id"),
+          // });
         } else {
           let attr = $(this).attr("id");
           if (typeof attr === typeof undefined || attr === false) {
             $(val).attr("id", "search-" + i);
           }
-          _this.index.addDoc({
-            id: i,
-            title: "",
-            body: $(val).text(),
-            link: $(val).attr("id"),
-          });
+          DocObj.id = i;
+          DocObj.title = "";
+          DocObj.body = $(val).text();
+          DocObj.link = $(val).attr("id");
+          // _this.index.addDoc({
+          //   id: i,
+          //   title: "",
+          //   body: $(val).text(),
+          //   link: $(val).attr("id"),
+          // });
         }
+        _this.index.push(DocObj);
       });
+      let options = {
+        // isCaseSensitive: false,
+        includeScore: true,
+        shouldSort: true,
+        includeMatches: true,
+        // findAllMatches: false,
+        minMatchCharLength: 3,
+        // location: 0,
+        threshold: 0.5,
+        // distance: 100,
+        // useExtendedSearch: false,
+        // ignoreLocation: false,
+        // ignoreFieldNorm: false,
+        keys: ["title", "body"],
+      };
+      this.fuse = new Fuse(this.index, options);
     },
 
     doFulltextSearch: function (e) {
       let _this = this;
       this.showSearchResults = true;
-      if (this.searchTerm !== "") {
-        this.searchResults = this.index.search(String(this.searchTerm), {
-          fields: {
-            title: { boost: 2 },
-            body: { boost: 1 },
-          },
-        });
-        this.searchResults = this.searchResults.map(function (res) {
-          let pos = _this.index.documentStore.docs[res.ref].body.indexOf(
-            _this.searchTerm
-          );
-          res.doc = new Object();
-          res.doc.link = _this.index.documentStore.docs[res.ref].link;
-          res.doc.short =
-            pos > 0
-              ? "... " +
-                _this.index.documentStore.docs[res.ref].body
-                  .substr(pos - 20 > 0 ? pos - 20 : 0, 40)
-                  .replace(
-                    _this.searchTerm,
-                    "<strong>" + _this.searchTerm + "</strong>"
-                  )
-              : _this.searchTerm;
-          //console.log(pos, _this.searchTerm, res.doc.short)
-          return res;
-        });
-              EventBus.publish('searchterm-entered', {searchTerm: this.searchTerm, searchResults: this.searchResults.length});
+      let index = this.fuse.getIndex();
 
-        // _this.$emit("log", "searchterm", {
-        //   searchterm: this.searchTerm,
-        //   results: this.searchResults.length,
-        // });
-      }
+      this.searchResults = this.fuse.search(this.searchTerm);
+
+      this.searchResults = this.searchResults.map(function (res) {
+        let changelist = [];
+        let pos = _this.index[res.refIndex].body.indexOf(_this.searchTerm);
+        res.doc = new Object();
+        res.doc.link = _this.index[res.refIndex].link;
+        if (res.item.body != "") {
+          if (pos > 0) {
+            res.doc.short =
+              "... " +
+              _this.index[res.refIndex].body
+                .substr(pos - 20 > 0 ? pos - 20 : 0, 40)
+                .replace(
+                  _this.searchTerm,
+                  "<strong>" + _this.searchTerm + "</strong>"
+                );
+          } else {
+            res.matches[0].indices.forEach((element) => {
+              changelist.push(
+                _this.index[res.refIndex].body.substr(
+                  element[0],
+                  element[1] - element[0] + 1
+                )
+              );
+            });
+            changelist.forEach((el) => {
+              _this.index[res.refIndex].body = _this.index[
+                res.refIndex
+              ].body.replace(el, "<strong>" + el + "</strong>");
+            });
+            res.doc.short = _this.index[res.refIndex].body;
+          }
+        } else if (res.item.title != "") {
+          if (pos > 0) {
+            res.doc.short =
+              "... " +
+              _this.index[res.refIndex].title
+                .substr(pos - 20 > 0 ? pos - 20 : 0, 40)
+                .replace(
+                  _this.searchTerm,
+                  "<strong>" + _this.searchTerm + "</strong>"
+                );
+          } else {
+            res.matches[0].indices.forEach((element) => {
+              changelist.push(
+                _this.index[res.refIndex].title.substr(
+                  element[0],
+                  element[1] - element[0] + 1
+                )
+              );
+            });
+            changelist.forEach((el) => {
+              _this.index[res.refIndex].title = _this.index[
+                res.refIndex
+              ].title.replace(el, "<strong>" + el + "</strong>");
+            });
+            res.doc.short = _this.index[res.refIndex].title;
+          }
+        }
+        return res;
+      });
+      EventBus.publish("searchterm-entered", {
+        searchTerm: this.searchTerm,
+        searchResults: this.searchResults.length,
+      });
       e.preventDefault();
     },
 
     searchResultClick: function (doc) {
-      //this.hideSearchResults();     disabled this functionality for now
-      // this.$emit("log", "searchresultselected", {
-      //   searchterm: this.term,
-      //   results: this.searchResults.length,
-      //   selected: doc.link,
-      //   title: doc.title,
-      // });
-      EventBus.publish('searchresult-selected', {searchTerm: this.searchTerm, searchResults: this.searchResults.length, link: doc.link, short: doc.short});
+      EventBus.publish("searchresult-selected", {
+        searchTerm: this.searchTerm,
+        searchResults: this.searchResults.length,
+        link: doc.link,
+        short: doc.short,
+      });
     },
 
     hideSearchResults: function () {
       this.showSearchResults = false;
     },
-    publishEvent(EventType){
+    publishEvent(EventType) {
       EventBus.publish(EventType);
     },
-    deleteSearchResults(){
+    deleteSearchResults() {
       this.searchResults = "";
       this.showSearchResults = false;
       //this.searchTerm = "";
-    }
+    },
   },
 };
 //@click="publishEvent(SidebarEvents.TOGGLE_TABS)"
@@ -150,12 +215,7 @@ export default {
         v-model="searchTerm"
         v-on:keyup.enter="doFulltextSearch"
         id="search-string"
-        class="
-          form-control form-control-sm
-          mr-sm-2
-          w-md-50 w-xs-75
-          ml-auto
-        "
+        class="form-control form-control-sm mr-sm-2 w-md-50 w-xs-75 ml-auto"
         type="search"
         placeholder="Suchen"
         aria-label="Search"
@@ -174,23 +234,21 @@ export default {
       class="h-100 w-md-50 w-xs-100 ml-auto px-0 mx-0"
       style="z-index: 3000"
     >
-      
       <div class="p-3 bg-light" style="max-height: 80vh; overflow: auto">
-        <div class = "d-flex">
-        <div class="mb-2 w-75">
-          {{ searchResults.length }} Suchtreffer für '{{ searchTerm }}':
-          
-        </div>
-        <div class="w-25">
-        <button
-          type="button"
-          class="close ml-auto align-self-center d-block"
-          aria-label="Close"
-          @click="deleteSearchResults()"
-        >
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
+        <div class="d-flex">
+          <div class="mb-2 w-75">
+            {{ searchResults.length }} Suchtreffer für '{{ searchTerm }}':
+          </div>
+          <div class="w-25">
+            <button
+              type="button"
+              class="close ml-auto align-self-center d-block"
+              aria-label="Close"
+              @click="deleteSearchResults()"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
         </div>
         <ul id="search-results" class="list-unstyled">
           <li class="mb-2" v-for="res in searchResults">
