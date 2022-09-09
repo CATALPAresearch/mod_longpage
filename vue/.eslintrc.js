108520ed980db3421e061998dc87d9564f76cdd7
nise <niels.seidel@nise81.com>
2022-09-09 16:01:52 +0200

    improved webpack config and package.json #247

--- vue/.eslintrc.js
+++ vue/.eslintrc.js
@@ -1,32 +1,13 @@
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * @package    mod_page
- * @copyright  2021 Adrian Stritzinger <Adrian.Stritzinger@studium.fernuni-hagen.de>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 module.exports = {
     env: {
         browser: true,
-        node: true,
+        //node: true,
+        //"es2015": true
     },
     extends: [
         'eslint:recommended',
-        'plugin:vue/vue3-recommended',
+        //'plugin:vue/vue3-recommended',
+        "plugin:vue/vue3-essential"
     ],
     globals: {
         $: 'readonly',
@@ -38,5 +19,15 @@ module.exports = {
         'object-shorthand': ['error'],
         'quote-props': ['error', 'as-needed'],
         quotes: ['error', 'single', {avoidEscape: true}],
+    },
+    "plugins": [
+        "vue"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     }
 };
+
+
+
