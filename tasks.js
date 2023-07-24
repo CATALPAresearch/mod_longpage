845639b515f1d88fa576c4806241273586814129
Dennis Menze <dennis.menze@fernuni-hagen.de>
2023-07-24 14:56:20 +0200

    Improved resizing of drag&drop tasks

---
+++ tasks.js
@@ -0,0 +1,73 @@
+function getClassnameNumericSuffix(node, prefix)
+    {
+        var classes = node.attr('class');
+        if (classes !== '') {
+            var classesArr = classes.split(' ');
+            for (var index = 0; index < classesArr.length; index++) {
+                var patt1 = new RegExp('^' + prefix + '([0-9])+$');
+                if (patt1.test(classesArr[index])) {
+                    var patt2 = new RegExp('([0-9])+$');
+                    var match = patt2.exec(classesArr[index]);
+                    return Number(match[0]);
+                }
+            }
+        }
+        return null;
+};
+    
+function resizeAllDragsAndDrops() {
+  var root = $(".ddimageortext");
+  root.find(".draghomes").prependTo(root.find(".ddarea"));
+  root.find(".draghomes > div").each(function (i, node) {
+    resizeAllDragsAndDropsInGroup(
+      getClassnameNumericSuffix($(node), "dragitemgroup"),
+      root
+    );
+  });
+}
+
+//modified from ddimageortext / question.js
+function resizeAllDragsAndDropsInGroup(group, root) {
+  var dragHomes = root.find(".dragitemgroup" + group + " .draghome"),
+    maxWidth = 0,
+    maxHeight = 0;
+  // Find the maximum size of any drag in this groups.
+  dragHomes.each(function (i, drag) {
+    maxWidth = Math.max(maxWidth, Math.ceil(drag.offsetWidth));
+    maxHeight = Math.max(maxHeight, Math.ceil(drag.offsetHeight));
+  });
+
+  // The size we will want to set is a bit bigger than this.
+  maxWidth += 10;
+  maxHeight += 10;
+
+  // Set each drag home to that size.
+  dragHomes.each(function (i, drag) {
+    var left = Math.round((maxWidth - drag.offsetWidth) / 2),
+      top = Math.floor((maxHeight - drag.offsetHeight) / 2);
+    // Set top and left padding so the item is centred.
+      
+     $(drag).css({
+      "padding-left": left + "px",
+      "padding-right": maxWidth - drag.offsetWidth - left + "px",
+      "padding-top": top + "px",
+      "padding-bottom": maxHeight - drag.offsetHeight - top + "px",
+     });
+
+    root
+      .find(".dropzone.place" + i + ".group" + group)
+      .width(maxWidth - 12)
+      .height(maxHeight - 12);
+  });
+}
+
+var observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+        resizeAllDragsAndDrops();
+        observer.unobserve(document.documentElement);
+    }
+  });
+});
+
+observer.observe(document.documentElement);
--- vue/src/components/LongpageSidebar/Quiz/index.vue
+++ vue/src/components/LongpageSidebar/Quiz/index.vue
@@ -246,70 +246,6 @@ export default {
       ]);
     }
 
-    //needed for drag & drop task types to resize placeholder correctly
-    $('#carousel').on('slid.bs.carousel', function (event) {
-      resizeAllDragsAndDrops();
-    })
-
-    function getClassnameNumericSuffix(node, prefix)
-    {
-        var classes = node.attr('class');
-        if (classes !== '') {
-            var classesArr = classes.split(' ');
-            for (var index = 0; index < classesArr.length; index++) {
-                var patt1 = new RegExp('^' + prefix + '([0-9])+$');
-                if (patt1.test(classesArr[index])) {
-                    var patt2 = new RegExp('([0-9])+$');
-                    var match = patt2.exec(classesArr[index]);
-                    return Number(match[0]);
-                }
-            }
-        }
-        return null;
-    };
-
-    function resizeAllDragsAndDrops()
-    {
-      var root = $("#question .active iframe").contents().find(".ddimageortext");
-      root.find(".draghomes").prependTo(root.find(".ddarea")); 
-      root.find('.draghomes > div').each(function(i, node) {
-            resizeAllDragsAndDropsInGroup(
-                    getClassnameNumericSuffix($(node), 'dragitemgroup'), root);
-        });
-    };
-
-    //modified from ddimageortext / question.js
-    function resizeAllDragsAndDropsInGroup(group, root)
-    {
-      var dragHomes = root.find('.dragitemgroup' + group + ' .draghome'),
-          maxWidth = 0,
-        maxHeight = 0;
-           // Find the maximum size of any drag in this groups.
-        dragHomes.each(function(i, drag) {
-            maxWidth = Math.max(maxWidth, Math.ceil(drag.offsetWidth));
-            maxHeight = Math.max(maxHeight, Math.ceil(drag.offsetHeight));
-        });
-
-        // The size we will want to set is a bit bigger than this.
-        maxWidth += 10;
-        maxHeight += 10;
-
-        // Set each drag home to that size.
-        dragHomes.each(function(i, drag) {
-            var left = Math.round((maxWidth - drag.offsetWidth) / 2),
-                top = Math.floor((maxHeight - drag.offsetHeight) / 2);
-            // Set top and left padding so the item is centred.
-            $(drag).css({
-                'padding-left': left + 'px',
-                'padding-right': (maxWidth - drag.offsetWidth - left) + 'px',
-                'padding-top': top + 'px',
-                'padding-bottom': (maxHeight - drag.offsetHeight - top) + 'px'
-            });
-
-            root.find('.dropzone.place' + i).width(maxWidth - 12).height(maxHeight - 12);
-        });
-    }
-
     function isElementInViewport(element, index, array)
     {
       // Special bonus for those using jQuery
@@ -398,6 +334,11 @@ export default {
                 cssLink.rel = "stylesheet"; 
                 cssLink.type = "text/css"; 
                 $(this).contents().find("head").append(cssLink);
+
+                var jsLink = document.createElement("script");
+                jsLink.src = window.location.href.substring(0, window.location.href.lastIndexOf('/')) + "/tasks.js"; 
+                $(this).contents().find("head").append(jsLink);
+
                 $("#quiz-spinner").remove();
                 $(this).contents().find("body").on('click', function (ev) {
                   var logentry = {
@@ -473,8 +414,6 @@ export default {
           $("#question .carousel-item:first").addClass("active");
           $("#carousel-indicators").children().remove();
           $(".carousel-control-prev, .carousel-control-next").hide();
-
-          resizeAllDragsAndDrops();
 
           if ($("#question").children().length > 1)
           {
